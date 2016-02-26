1731c15ec3c927a71cb8df1deacb484ca000b575
RegisOB <regismaurin.obiangmba@gmail.com>
2016-02-26 01:50:33 +0100

    Add table for absence

--- R/TimeSheet.R
+++ R/TimeSheet.R
@@ -27,19 +27,52 @@ TableProgram <- as.data.frame(TableProgram)
 for (i in 1:dim(TableProgram)[1]){
   
   for (j in 1:dim(TableProgram)[2]) {
-    TableProgram[i,j] <- ifelse(TS_data.0[22, ColumnDays[j]]=='8', 8, 0)*TS_data.0[22, ColumnProject[i]]/100
-    
+    TableProgram[i,j] <- ifelse(TS_data.0[1, ColumnDays[j]]=='8', 8, 0)*TS_data.0[1, ColumnProject[i]]/100
+    colnames(TableProgram) <- as.character(1:length(Days))
   }
 }
 
+##Modified names of columns/rows
+colnames(TableProgram) <- as.character(1:length(Days))
+row.names(TableProgram) <- names(TS_data.0)[ColumnProject]
+
+##Added column called total
+SumCol <- as.data.frame(apply(TableProgram, 1, sum))
+names(SumCol) <- 'Total'
+TableProgram.1 <- cbind(TableProgram, SumCol)
+
+##Added column called total
+SumRow <- as.data.frame(apply(TableProgram.1, 2, sum))
+names(SumRow) <- 'Total'
+TableProgram.2 <- rbind(TableProgram.1, t(SumRow))
 
 
+###Creation table for Week(W), Annual leave (A), Public hollidays (P), Illness (I), Other absence (O)
+#####################################################################################################
+TableAbsence <- matrix(0, nrow = 5, ncol = length(Days))
+TableAbsence <- as.data.frame(TableAbsence)
 
+row.names(TableAbsence) <- c('Weekends', 'Annual leave', 'Public holidays', 'Illness', 'Other absence')
+colnames(TableAbsence) <- as.character(1:length(Days))
 
+##For one employee
+List.absence <- c('W','A','P','I','O')
+for (i in 1:dim(TableAbsence)[1]){
+  
+  for (j in 1:dim(TableAbsence)[2]) {
+    TableAbsence[i,j] <- ifelse(TS_data.0[1, ColumnDays[j]]==List.absence[i], 8, 0)
+  }
+}
 
+##Added column called total
+SumCol <- as.data.frame(apply(TableAbsence, 1, sum))
+names(SumCol) <- 'Total'
+TableAbsence.1 <- cbind(TableAbsence, SumCol)
 
-
-
+##Added column called total
+SumRow <- as.data.frame(apply(TableAbsence.1, 2, sum))
+names(SumRow) <- 'Total'
+TableAbsence.2 <- rbind(TableAbsence.1, t(SumRow))
 
 
 
